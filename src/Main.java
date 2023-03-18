d4dd75eed7ac54e04d5fc0e792ecbeb7a1927599
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 18:11:09 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,41 @@ import java.util.Scanner;
 
 public class Main {
     static boolean magical(int[][] arr) {
-        // ...
+        int d1 = 0;
+        int d2 = 0;
+
+        for (int i=0; i < arr.length; i++) {
+            if (arr[i].length != arr.length) {
+                return false;
+            }
+            d1 += arr[i][i];
+            d2 += arr[i][arr.length-1-i];
+        }
+
+        if (d1 != d2) {
+            return false;
+        }
+
+        for (int i=0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+            if (sum != d1) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr.length; j++) {
+                sum += arr[j][i];
+            }
+            if (sum != d1) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
